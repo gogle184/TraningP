937b80e72e892ddda666bad77701aaf71e6c39e4
佐藤元紀 <satougenki@satoumotonorinoMacBook-Pro.local>
2023-04-27 17:21:29 +0900

    メインページ画像追加。保留

--- app/models/content.rb
+++ app/models/content.rb
@@ -2,4 +2,6 @@ class Content < ApplicationRecord
   belongs_to :user
   has_rich_text :text
   belongs_to :category
+  mount_uploader :video, VideoUploader
+  mount_uploader :image, ActionTextImageUploader
 end
