15e7dda400d91a5c29b0f9356d2031d9615f6de0
佐藤元紀 <satougenki@satoumotonorinoMacBook-Pro.local>
2023-04-30 19:59:39 +0900

    add バリデーション、投稿成功時のflashメッセージ

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,8 +11,10 @@ class CategoriesController < ApplicationController
     @category.admin_id = current_user.admin_id
     @category.user_id = current_user.id
     if @category.save
+      flash[:notice] = "新規作成に成功しました"
       redirect_to categories_path
     else
+      flash[:alert] = "エラーを確認してください"
       @categories = Category.where(admin_id: current_user.admin_id)
       render 'index'
     end
@@ -23,14 +25,17 @@ class CategoriesController < ApplicationController
 
   def update
     if @category.update(category_params)
+      flash[:notice] = "編集を反映しました"
       redirect_to categories_path
     else
+      flash[:alert] = "エラーを確認してください"
       render 'edit'
     end
   end
 
   def destroy
     @category.destroy
+    flash[:notice] = "カテゴリーを削除しました"
     redirect_to categories_path
   end
 
