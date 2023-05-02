707c4f5cbe17ac515dee53900a405f5477672344
佐藤元紀 <satougenki@satoumotonorinoMacBook-Pro.local>
2023-05-02 11:10:24 +0900

    add contentモデルにyoutube_urlカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_01_083853) do
+ActiveRecord::Schema.define(version: 2023_05_02_020915) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 2023_05_01_083853) do
     t.string "video"
     t.string "admin_id", default: "", null: false
     t.string "image"
+    t.string "youtube_url"
     t.index ["user_id"], name: "index_contents_on_user_id"
   end
 
