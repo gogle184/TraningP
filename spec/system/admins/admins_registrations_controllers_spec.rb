c88a524f997e2415ccefb9d2dd9efcfbf5d888e0
佐藤元紀 <satougenki@satoumotonorinoMacBook-Pro.local>
2023-05-05 21:03:48 +0900

    fix admin認証のテスト追加、fix admin編集ページの編集

---
+++ spec/system/admins/admins_registrations_controllers_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "Admins::RegistrationsControllers", type: :system do
+  before do
+    @admin = create(:admin)
+  end
+
+  scenario 'admin(管理者)が新規登録できること' do
+    visit new_admin_registration_path
+    fill_in 'admin[email]', with: 'new@example.com'
+    fill_in 'admin[password]', with: 'password'
+    fill_in 'admin[password_confirmation]', with: 'password'
+    fill_in 'admin[project_id]', with: 'admin1'
+    click_button 'アカウントを作成する'
+  
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'アカウント登録が完了しました'
+  end
+
+  scenario 'admin(管理者)が情報を編集できること' do
+    login_as(@admin)
+    visit edit_admin_registration_path
+    fill_in 'admin[email]', with: 'new@example.com'
+    fill_in 'admin[current_password]', with: 'password' 
+    click_button '変更を保存する'
+  
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'アカウント情報を変更しました'
+  end
+  
+end
