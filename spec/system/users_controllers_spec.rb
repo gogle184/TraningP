e96d77419a2538173c323738a970a77cf9c9cb2b
佐藤元紀 <satougenki@satoumotonorinoMacBook-Pro.local>
2023-05-06 15:09:45 +0900

    add admin,userのshow項目にてsystemSpec導入

--- spec/system/users_controllers_spec.rb
+++ spec/system/users_controllers_spec.rb
@@ -1,9 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "UsersControllers", type: :system do
-  before do
-    driven_by(:rack_test)
-  end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  let(:user) {create(:user)}
+
+  context 'user(利用者)の詳細が見れること' do
+    scenario '詳細ページに情報があること' do
+      visit new_user_session_path
+      fill_in 'user[email]', with: user.email
+      fill_in 'user[password]', with: user.password
+      click_button 'ログイン'
+      expect(current_path).to eq root_path
+      expect(page).to have_content 'ログインしました'
+      click_link 'Profile'
+      expect(current_path).to eq user_path(user)
+      expect(page).to have_content(user.email)
+      expect(page).to have_content(user.project_id)
+      expect(page).to have_content 'アカウントを編集する'
+    end
+  end
 end
